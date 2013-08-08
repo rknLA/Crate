f164e0ea1190cda45250f1c41a1473c26cc66b9c
R. Kevin Nelson <kevin@rkn.la>
2013-08-07 22:47:57 -0700

    Initial Commit

---
+++ PlayWithOthers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlayWithOthers
+//
+//  Created by Kevin Nelson on 8/7/13.
+//  Copyright (c) 2013 R. Kevin Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PWOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PWOAppDelegate class]));
+  }
+}
---
+++ PlayWithOthersTests/PlayWithOthersTests.h
@@ -0,0 +1,13 @@
+//
+//  PlayWithOthersTests.h
+//  PlayWithOthersTests
+//
+//  Created by Kevin Nelson on 8/7/13.
+//  Copyright (c) 2013 R. Kevin Nelson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PlayWithOthersTests : SenTestCase
+
+@end
